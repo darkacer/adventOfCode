df20c855cfdef0cb4a3d47ef991332de3e3db145
darkacer <omkardeokar95@gmail.com>
2021-01-30 16:33:19 +0530

    day 13

---
+++ Year2020/Day13/day13-2improv.ts
@@ -0,0 +1,18 @@
+import { data } from "./data.ts"; 
+let user = new data();
+
+let inputArr = user.returnArray().map(el => (el != 'x') ? parseInt(el) : 0)
+let num = 0;
+let incrementBy = 1;
+
+for(let i = 0; i < inputArr.length; i++) {
+    if(i != 0 && inputArr[i - 1]) incrementBy *= inputArr[i - 1]
+    if(inputArr[i] === 0) continue
+    while(true) {
+        num += incrementBy
+        if(!((num + i) % inputArr[i])) {
+            break
+        }
+    }
+}
+console.log(num);
